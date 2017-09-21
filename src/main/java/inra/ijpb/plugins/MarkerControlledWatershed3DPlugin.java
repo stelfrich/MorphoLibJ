0077686555263d4532b61b0b4e0be8ef12e4f61d
Stefan Helfrich <stefan.helfrich@googlemail.com>
2017-09-21 17:39:01 +0200

    Make MCW3DPlugin a SciJava Command

--- src/main/java/inra/ijpb/plugins/MarkerControlledWatershed3DPlugin.java
+++ src/main/java/inra/ijpb/plugins/MarkerControlledWatershed3DPlugin.java
@@ -21,11 +21,13 @@
  */
 package inra.ijpb.plugins;
 
-import ij.IJ;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
 import ij.ImagePlus;
-import ij.WindowManager;
-import ij.gui.GenericDialog;
-import ij.plugin.PlugIn;
 import inra.ijpb.binary.BinaryImages;
 import inra.ijpb.data.image.Images3D;
 import inra.ijpb.watershed.Watershed;
@@ -40,17 +42,36 @@ import inra.ijpb.watershed.Watershed;
  *
  * @author Ignacio Arganda-Carreras
  */
-public class MarkerControlledWatershed3DPlugin implements PlugIn 
+@Plugin(type = Command.class, menuPath = "Plugins>MorphoLibJ>Segmentation>Marker-controlled Watershed")
+public class MarkerControlledWatershed3DPlugin implements Command
 {
+	@Parameter
+	private ImagePlus input;
+	
+	@Parameter
+	private ImagePlus marker;
+	
+	@Parameter(required = false)
+	private ImagePlus mask;
+	
+	@Parameter(type=ItemIO.OUTPUT)
+	private ImagePlus result;
+	
 	/** flag set to TRUE if markers are binary, to FALSE if markers are labels */
-	public static boolean binaryMarkers = true;
+	@Parameter
+	private boolean binaryMarkers = true;
 	
 	/** flag to calculate watershed dams */
-	public static boolean getDams = true;
+	@Parameter
+	private boolean getDams = true;
 	
 	/** flag to use 26-connectivity */
-	public static boolean use26neighbors = true;
-		
+	@Parameter
+	private boolean use26neighbors = true;
+
+	@Parameter
+	LogService logService;
+	
 	/**
 	 * Apply marker-controlled watershed to a grayscale 2D or 3D image.
 	 *	 
@@ -60,6 +81,7 @@ public class MarkerControlledWatershed3DPlugin implements PlugIn
 	 * @param connectivity 6 or 26 voxel connectivity
 	 * @return the resulting watershed
 	 */
+	@SuppressWarnings( "hiding" )
 	public ImagePlus process(
 			ImagePlus input, 
 			ImagePlus marker,
@@ -70,93 +92,37 @@ public class MarkerControlledWatershed3DPlugin implements PlugIn
 		
 		if (binaryMarkers)
 		{
-			IJ.log("-> Compute marker labels");
+			logService.info( "-> Compute marker labels" );
 			marker = BinaryImages.componentsLabeling(marker, connectivity, 32);
 		}
 		
-		IJ.log("-> Running watershed...");
-								
-		ImagePlus resultImage = Watershed.computeWatershed(input, marker, mask, connectivity, getDams );				
+		logService.info("-> Running watershed...");
+		
+		ImagePlus resultImage = Watershed.computeWatershed(input, marker, mask, connectivity, getDams );
 		
 		final long end = System.currentTimeMillis();
-		IJ.log( "Watershed 3d took " + (end-start) + " ms.");		
-						
-		return resultImage;				
+		logService.info( "Watershed 3d took " + (end-start) + " ms.");
+		
+		return resultImage;
 	}
 	
 
-	/**
-	 * Plugin run method to be called from ImageJ
-	 */
 	@Override
-	public void run(String arg) 
+	public void run()
 	{
-		int nbima = WindowManager.getImageCount();
-		
-		if( nbima < 2 )
-		{
-			IJ.error( "Marker-controlled Watershed", 
-					"ERROR: At least two images need to be open to run Marker-controlled Watershed.");
-			return;
-		}
-		
-        String[] names = new String[ nbima ];
-        String[] namesMask = new String[ nbima + 1 ];
+		// a 3D image is assumed but it will use 2D connectivity if the
+		// input is 2D
+		int connectivity = use26neighbors ? 26 : 6;
+		if ( input.getImageStackSize() == 1 )
+			connectivity = use26neighbors ? 8 : 4;
 
-        namesMask[ 0 ] = "None";
-        
-        for (int i = 0; i < nbima; i++) 
-        {
-            names[ i ] = WindowManager.getImage(i + 1).getShortTitle();
-            namesMask[ i + 1 ] = WindowManager.getImage(i + 1).getShortTitle();
-        }
-        
-        GenericDialog gd = new GenericDialog("Marker-controlled Watershed");
+		result = process( input, marker, mask, connectivity );
 
-        int inputIndex = 0;
-        int markerIndex = nbima > 1 ? 1 : 0;
-        
-        gd.addChoice( "Input", names, names[ inputIndex ] );
-        gd.addChoice( "Marker", names, names[ markerIndex ] );
-        gd.addChoice( "Mask", namesMask, namesMask[ 0 ] );
-        gd.addCheckbox("Binary markers", true);
-        gd.addCheckbox( "Calculate dams", getDams );
-        gd.addCheckbox( "Use diagonal connectivity", use26neighbors );
+		// Set result slice to the current slice in the input image
+		result.setSlice( input.getCurrentSlice() );
 
-        gd.showDialog();
-        
-        if (gd.wasOKed()) 
-        {
-            inputIndex = gd.getNextChoiceIndex();
-            markerIndex = gd.getNextChoiceIndex();
-            int maskIndex = gd.getNextChoiceIndex();
-            binaryMarkers = gd.getNextBoolean();
-            getDams = gd.getNextBoolean();
-            use26neighbors = gd.getNextBoolean();
-
-            ImagePlus inputImage = WindowManager.getImage( inputIndex + 1 );
-            ImagePlus markerImage = WindowManager.getImage( markerIndex + 1 );
-            ImagePlus maskImage = maskIndex > 0 ? WindowManager.getImage( maskIndex ) : null;
-            
-            // a 3D image is assumed but it will use 2D connectivity if the
-            // input is 2D
-            int connectivity = use26neighbors ? 26 : 6;
-            if( inputImage.getImageStackSize() == 1 )
-            	connectivity = use26neighbors ? 8 : 4;
-
-            ImagePlus result = process( inputImage, markerImage, maskImage, connectivity );
-                                    
-    		// Set result slice to the current slice in the input image
-            result.setSlice( inputImage.getCurrentSlice() );
-            
-            // optimize display range
-            Images3D.optimizeDisplayRange( result );
-            
-            // show result
-            result.show();
-        }
-		
+		// optimize display range
+		Images3D.optimizeDisplayRange( result );
 	}
 
 }
-
